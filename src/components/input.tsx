2058131a0ed31ae95a7a348b3a679470d7816c5a
augusto.dev@proton.me <augusto.dev@proton.me>
2023-08-03 14:53:38 -0300

    refactor: :recycle: Refactor input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,13 +6,14 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const InputComponent: React.FC<InputProps> = ({
   label,
+  ...rest
 }) => {
   return (
     <div className="flex">
       <div className=' bg-gray-400 p-5 rounedd-l-lg'>
         <p className="mr-2 decoration-gray-600 font-medium">{label}</p>
       </div>
-      <input className='flex-1 border-solid border-2 pr-1'/>
+      <input className='flex-1 border-solid border-2 pr-1' {...rest}/>
     </div>
   );
 };
