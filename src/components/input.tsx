9b08375fc97e37dd9f13e12a8b9aea5f4c84c8cd
augusto.dev@proton.me <augusto.dev@proton.me>
2023-08-05 18:02:30 -0300

    refactor: :recycle: Refactor input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -8,13 +8,13 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const InputComponent: React.FC<InputProps> = ({ placeholder, onClickPlus, ...rest }) => {
   return (
-    <div className="max-w-md w-full px-4">
+    <div className="max-w-md w-full py-6">
       <div className="relative rounded-md">
         <input
           {...rest}
           type="text"
           placeholder={placeholder}
-          className="w-full py-2 px-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-500 focus:border-blue-500"
+          className="w-full py-2 pl-10 pr-3 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
         />
         <div
           className="absolute inset-y-0 right-0 pl-3 flex items-center cursor-pointer"
