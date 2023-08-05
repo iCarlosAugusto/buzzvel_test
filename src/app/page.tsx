bd97a6b00c5074e01e8cc075c4d014263abcb290
augusto.dev@proton.me <augusto.dev@proton.me>
2023-08-05 12:57:10 -0300

    feat: :sparkles: Create and reading tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,56 @@
 "use client";
 
 import { InputComponent } from "@/components/input";
+import axios from "axios";
 import { Formik } from "formik";
+import { useEffect, useState } from "react";
 import * as Yup from "yup";
 
 interface InitialValuesProps {
-  name: string;
-  github: string;
-  linkedin: string;
+  title: string;
+  message: string;
 }
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   const schema = Yup.object().shape({
-    name: Yup.string().required("Campo obrigatório"),
-    github: Yup.string().required("Campo obrigatório"),
-    linkedin: Yup.string().required("Campo obrigatório"),
+    title: Yup.string().required("Campo obrigatório"),
+    message: Yup.string().required("Campo obrigatório"),
   });
 
   const initialValues: InitialValuesProps = {
-    name: "",
-    github: "",
-    linkedin: "",
+    title: "",
+    message: "",
   };
 
-  const subitForm = (values: InitialValuesProps) => {
-    console.log(values);
+  const subitForm = async (values: InitialValuesProps) => {
+    const { data: task} = await axios.post("http://localhost:3000/task/create", {
+      title: values.title,
+      message: values.message,
+    });
+    const newTasks: Task[] = [...tasks, task];
+    setTasks(newTasks);
   };
+
+  const fetchAllTasks = async () => {
+    const { data } = await axios.get("http://localhost:3000/task/getAll");
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, []);
 
   return (
     <main>
-      <p> QR Code Generator </p>
+      <div className="flex justify-between">
+        <p> Olá, Carlos! O que vamos resolver hoje?</p>
+        <InputComponent label="Títutlo" value={""} onChange={() => {}} />
+        <button> Pesquisar</button>
+      </div>
+
+      <p className="text-2xl"> O vamos criar? </p>
       <Formik
         onSubmit={subitForm}
         initialValues={initialValues}
@@ -38,26 +59,25 @@ export default function Home() {
         {({ handleChange, values, handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <InputComponent
-              label="name"
-              value={values.name}
-              onChange={handleChange("name")}
+              label="Títutlo"
+              value={values.title}
+              onChange={handleChange("title")}
             />
             <InputComponent
-              label="LinkedIn"
-              value={values.linkedin}
-              onChange={handleChange("linkedin")}
+              label="Mensagem"
+              value={values.message}
+              onChange={handleChange("message")}
             />
-            <InputComponent
-              label="Github"
-              value={values.github}
-              onChange={handleChange("github")}
-            />
-            <button type="submit"> 
-              Generate Image
-            </button>
+            <button type="submit">Create</button>
           </form>
         )}
       </Formik>
+
+      <p className="text-2xl"> To-do criadas </p>
+
+      {tasks.map((task, index) => (
+        <p key={index}> {task.title}</p>
+      ))}
     </main>
   );
 }
