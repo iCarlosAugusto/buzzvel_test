1b70b9abb0e55c3a78db058950896805dd13272c
augusto.dev@proton.me <augusto.dev@proton.me>
2023-08-06 17:54:43 -0300

    refactor: :recycle: Resposive

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,29 +6,31 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   onClickPlus: () => void;
 }
 
-export const InputComponent: React.FC<InputProps> = ({ placeholder, onClickPlus, ...rest }) => {
+export const InputComponent: React.FC<InputProps> = ({
+  placeholder,
+  onClickPlus,
+  ...rest
+}) => {
   return (
-    <div className="w-full py-6">
-      <div className="relative rounded-md">
-        <input
-          {...rest}
-          type="text"
-          placeholder={placeholder}
-          className="w-full py-2 pl-2 pr-10 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+    <div className="relative rounded-md">
+      <input
+        {...rest}
+        type="text"
+        placeholder={placeholder}
+        className="w-full py-2 pl-2 pr-10 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+      />
+      <div
+        className="absolute inset-y-0 right-2 pl-3 flex items-center cursor-pointer"
+        onClick={onClickPlus}
+      >
+        <Image
+          src="plus-circle.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={24}
+          height={24}
+          priority
         />
-        <div
-          className="absolute inset-y-0 right-2 pl-3 flex items-center cursor-pointer"
-          onClick={onClickPlus}
-        >
-          <Image
-            src="plus-circle.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={24}
-            height={24}
-            priority
-          />
-        </div>
       </div>
     </div>
   );
--- src/components/search_input.tsx
+++ src/components/search_input.tsx
@@ -5,26 +5,27 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-export const InputSearchComponent: React.FC<InputProps> = ({ label, ...rest }) => {
+export const InputSearchComponent: React.FC<InputProps> = ({
+  label,
+  ...rest
+}) => {
   return (
-    <div className="w-full py-6">
-      <div className="relative">
-        <input
-          {...rest}
-          type="text"
-          placeholder="Search for task..."
-          className="w-full py-2 pl-2 pr-10 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+    <div className="relative py-6">
+      <input
+        {...rest}
+        type="text"
+        placeholder="Search for task..."
+        className="w-full py-2 pl-2 pr-10 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+      />
+      <div className="absolute inset-y-0 right-2 pl-3 flex items-center pointer-events-none">
+        <Image
+          src="search.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={24}
+          height={24}
+          priority
         />
-        <div className="absolute inset-y-0 right-2 pl-3 flex items-center pointer-events-none">
-          <Image
-            src="search.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={24}
-            height={24}
-            priority
-          />
-        </div>
       </div>
     </div>
   );
